c17bd40d5eda4bef54b5d0da0ec66bbfb50ef122
VinTran <tranquillo.vinnie@gmail.com>
2019-06-11 21:23:51 -0400

    react issues

--- timez/client/src/App.js
+++ timez/client/src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/login";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          {/* <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} /> */}
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
